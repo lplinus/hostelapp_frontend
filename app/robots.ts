c1d6057adac9e4bc2083953a4e575ccb319c8b50
linus145 <lakklplbros@gmail.com>
2026-03-20 16:51:35 +0530

    robot updated

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,5 @@
 export default function robots() {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://staynest.in";
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://hostelin.online";
 
     return {
         rules: [
